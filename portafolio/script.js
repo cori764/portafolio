ec63e44085836c94b65103a35901682f925b59d2
cori764 <williamhuanuco27@gmail.com>
2025-04-24 17:03:23 -0500

    Add files via upload

---
+++ portafolio/script.js
@@ -0,0 +1,83 @@
+/*============================= Alternar icono de barra de navegación (SCROLL) ==========================================*/
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+/*============================= Fin Alternar icono de barra de navegación (SCROLL) ==========================================*/
+
+/*============================= Desplazamiento del navbar por secciones con el enlace activo ==========================================*/
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /*==================== Navbar Pegajosa ======================*/
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*==================== Fin Navbar Pegajosa ======================*/
+
+    /*====== Eliminar el icono de alternar y la barra de navegación links (SCROLL)=========*/
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    /*====== Fin eliminar el icono de alternar y la barra de navegación links (SCROLL)==========*/
+};
+
+/*============================= Fin Desplazamiento del navbar por secciones con el enlace activo ==========================================*/
+
+/*============================= Animación (Scroll Reveal)==========================================*/
+
+ScrollReveal({
+
+    /* reset: true,*/
+
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+/*============= Animación Contenido ===============*/
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/*========== Fin Animación Contenido ============*/
+
+/*============================= Fin Animación (Scroll Reveal)==========================================*/
+
+/*============================= Animación Texto ( Type.JS )==========================================*/
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Programador Web', 'Diseñador  Web', 'Animador Grafico (3D)', 'YouTuber'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+/*============================= Fin Animación Texto ( Type.JS)==========================================*/
